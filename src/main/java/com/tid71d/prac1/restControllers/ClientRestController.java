448417fdfbe59895f93208c47968b455ec3703d2
Arturo Muela <eltorturo93@gmail.com>
2021-10-06 21:32:02 -0600

    added RestControllers

---
+++ src/main/java/com/tid71d/prac1/restControllers/ClientRestController.java
@@ -0,0 +1,48 @@
+package com.tid71d.prac1.restControllers;
+
+import com.tid71d.prac1.models.Client;
+import com.tid71d.prac1.services.ClientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientRestController {
+
+    private final ClientService clientService;
+
+    public ClientRestController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/{id}")
+    public Client findById(@PathVariable("id") Integer id){
+        try {
+            return clientService.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @GetMapping("/all")
+    public List<Client> findAll(){
+        return clientService.findAll();
+    }
+
+    @PostMapping("/save")
+    public Client save(Client client){
+        return clientService.save(client);
+    }
+
+    @PutMapping("/update/{id}")
+    public Client update(@PathVariable("id") Integer id,Client client){
+        try {
+            return clientService.update(id,client);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/tid71d/prac1/restControllers/ToolRestController.java
@@ -0,0 +1,48 @@
+package com.tid71d.prac1.restControllers;
+
+import com.tid71d.prac1.models.Client;
+import com.tid71d.prac1.models.Tool;
+import com.tid71d.prac1.services.ToolService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tools")
+public class ToolRestController {
+    private final ToolService toolService;
+
+    public ToolRestController(ToolService toolService) {
+        this.toolService = toolService;
+    }
+
+    @GetMapping("/{id}")
+    public Tool findById(@PathVariable("id") Integer id){
+        try {
+            return toolService.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @GetMapping("/all")
+    public List<Tool> findAll(){
+        return toolService.findAll();
+    }
+
+    @PostMapping("/save")
+    public Tool save(Tool tool){
+        return toolService.save(tool);
+    }
+
+    @PutMapping("/update/{id}")
+    public Tool update(@PathVariable("id") Integer id,Tool tool){
+        try {
+            return toolService.update(id,tool);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
